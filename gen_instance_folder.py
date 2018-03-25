24b2672ae3d66fcbf9c846c02cac2e27e9ae0779
Ethan Wong <epicface92@gmail.com>
2018-03-25 01:15:31 -0500

    Added feature to delete instance folder with gen_instance_folder.py

--- gen_instance_folder.py
+++ gen_instance_folder.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 """
 Short Python script to generate the instance folder which stores the config and databases
 """
@@ -10,12 +11,17 @@ config = \
 }"""
 
 import os
+from shutil import rmtree
 
 # Check if instance folder has already been created
 instance_folder = os.path.join(os.getcwd(), "instance")
 if os.path.isdir(instance_folder):
-    print("Instance folder has already been created!")
-    exit()
+    print("Instance folder has already been created! Do you want do delete and regenerate?")
+    if input("type 'yes' to regenerate: ") == "yes":
+        rmtree(instance_folder)
+        print("Deleted old instance")
+    else:
+        exit()
 
 # make instance folder
 os.mkdir(instance_folder)
@@ -26,4 +32,5 @@ with open(os.path.join(instance_folder, "config.json"), "w") as cfg:
     cfg.write(config)
 print("Generated config file")
 
+print("Please edit the outputted config.json file before running the web app!")
 print("NOTE: Databases are generated on first-run of website")
